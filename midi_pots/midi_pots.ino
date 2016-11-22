40afcd4751681de6dcb96e117445328bfd3338c8
Jesse Mejia <jmejia@anestheticaudio.com>
2016-11-21 21:55:08 -0800

    work onsite
    got pots working
    fixed some fade and channel behaviors
    ditched the music eq

---
+++ midi_pots/midi_pots.ino
@@ -0,0 +1,33 @@
+int pot1 = 14; 
+int val1 = 0;       
+int raw1 = 0;
+
+int pot2 = 15; 
+int val2 = 0;       
+int raw2 = 0;
+
+int pot3 = 16; 
+int val3 = 0;       
+int raw3 = 0;
+const int channel = 1;
+
+
+void setup() {
+}
+
+void loop() {
+  raw1 = analogRead(pot1);    // read the value from the sensors
+  raw2 = analogRead(pot2);    
+  raw3 = analogRead(pot3); 
+     
+  val1 = map(raw1, 0, 1023, 0, 127); // map sensors to 7-bit midi values
+  val2 = map(raw2, 0, 1023, 0, 127);
+  val3 = map(raw3, 0, 1023, 0, 127);
+  
+  usbMIDI.sendControlChange(1, val1, 1); // send each mapped value as a midi CC
+  usbMIDI.sendControlChange(2, val2, 1);
+  usbMIDI.sendControlChange(3, val3, 1);
+
+  delay(100);                  // slow your roll
+}
+ 
--- potentiometer_test/potentiometer_test.ino
+++ potentiometer_test/potentiometer_test.ino
@@ -1,7 +1,7 @@
-int potPin = 14; 
+int potPin = 15; 
 int val = 0;       
 int raw = 0;
-const int channel = 1
+const int channel = 1;
 
 
 void setup() {
@@ -10,7 +10,7 @@ void setup() {
 void loop() {
   raw = analogRead(potPin);    // read the value from the sensor
   val = map(raw, 0, 1023, 0, 127);
-  usbMIDI.sendControlChange(7, val, 1);
+  usbMIDI.sendControlChange(2, val, 1);
   delay(100);                  // stop the program for some time
 }
  
